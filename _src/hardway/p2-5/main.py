5420376352b3e774ccf977b0dbf22f584b8be6d2
LishuaiBeijing <lishuai.beijing@gmail.com>
2015-10-26 13:56:54 +0800

    hardway p2-5

---
+++ _src/hardway/p2-5/main.py
@@ -0,0 +1,15 @@
+﻿# -*- coding:utf-8 -*-
+# 这是一段注释
+print "Hi # 这不是注释"
+print "Rooters",25 + 30/5
+
+# 数字计算，整数与浮点数，比较大小
+print 3 + 2 - 4 % 3 + 1 / 5 + 6
+print 3 + 2 < 5 - 7
+print 3 + 2 - 4 % 3 + 1.0/5 + 6
+print "What's 3 + 2 ?", 3 + 2
+print "Is it greater?", 5 > -2
+
+# 中文测试
+str = "这是中文"
+print str
